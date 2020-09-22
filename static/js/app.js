2e582fe0bd8bd69f91756c4afebbdcfb08a00b5a
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-09-21 22:33:19 -0400

    Code for bar and bubble charts:

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,4 @@
 
-// function demographicInfo(){
-//   var metadataInfo = d3.select("#sample-metadata");
-//   d3.json("data/samples.json").then((incomingData) => {
-//     // Clear the node if data is present
-//     metadataInfo.html("");
-//     Object.entries(incomingData.metadata[0]).forEach(([key, value]) => {
-//       metadataInfo.append("h6").text(`${key} : ${value}`);
-//   });
-// });
-// };
-
-
 function demographicInfo(sample){
   var metadataInfo = d3.select("#sample-metadata");
   d3.json("data/samples.json").then((incomingData) => {
@@ -25,122 +13,49 @@ function demographicInfo(sample){
 });
 };
 
-
-
-
-
-// function dropdownData(){
-//   var dropdown = d3.select("#selDataset");
-//   d3.json("data/samples.json").then(incomingData => {
-//     dropdown.html("");
-//     Object.entries(incomingData.names).forEach(([key, value]) => {
-//       dropdown.append("p").text(`${value}`).append("hr")
-//   });
-//   })
-// }
-// dropdownData()
-
-function init(){
-  var selector = d3.select("#selDataset");
+function pieChart(barId){
   d3.json("data/samples.json").then(incomingData => {
-    var sampleNames = incomingData.names;
-    sampleNames.forEach(sample => {
-      selector.append("option").text(sample).property("value", sample);
-      
-    });
-    var firstSample = sampleNames[0];
-    console.log(firstSample);
-    demographicInfo(firstSample);
-
     
+    var sampleData = incomingData.samples;
+    var sampleList = sampleData.filter(sampleObject => sampleObject.id == barId);    
+    // console.log(sampleList);
+    var finalList = sampleList[0];
+
+    // console.log(finalList);
+    // var finalSlice = sampleList.slice(0, 10).reverse();
+    // console.log(finalSlice)
+    var otuIds = finalList.otu_ids.slice(0, 10).reverse();
+    var sampleValues = finalList.sample_values.slice(0, 10).reverse();
+    var otuLables = finalList.otu_labels.slice(0, 10).reverse();
+
+    // console.log(otuIds)
+    // console.log(sampleValues)
+    // console.log(otuLables)
+    trace = {
+      type: "bar",
+      x: sampleValues,
+      y: `OTU ${otuIds}`,
+      // value: sampleValues,
+      // label: otuIds,
+      text: otuLables
+    }
+
+    Plotly.newPlot("bar", [trace]);
   });
 }
 
-function optionChanged(changedSample){
-  demographicInfo(changedSample);
-
-
-}
-
-init()
-// d3.json("data/samples.json").then((incomingData) => console.log(incomingData));
-
-
-// function bubbleChart(){
-//     d3.json("data/samples.json").then(function(data) {
-
-//         var sampleArray = data.samples;
-
-//         var otuId = [];
-//         var sampleValue = [];
-//         var otuLabel = [];
-//         for (i = 0; i < sampleArray.length; i++) {
-            
-//             otuId.push(sampleArray[i].otu_ids);
-//             sampleValue.push(sampleArray[i].sample_values);
-//             otuLabel.push(sampleArray[i].otu_labels);   
-//           }
-        
-//           var otuIds = [].concat.apply([], otuId);
-//           var sampleValues = [].concat.apply([], sampleValue);
-//           var otuLabels = [].concat.apply([], otuLabel);
-
-//           // console.log(otuIds);
-//           // console.log(sampleValues);
-//           // console.log(otuLabels);
-//           var trace = {
-//             x: otuIds,
-//             y: sampleValues,
-//             mode: "markers",
-//             marker: {
-//                 color: otuIds,
-   
-//                 size: sampleValues,
-//                 sizeref: 2,
-//                 sizemode: 'area'
-//               },
-//             text: otuLabels
-            
-
-//          };
-//         Plotly.newPlot("bubble", [trace]);
-
-
-
-//         // for (i = 0; i< otuIds.length; i++) {
-//         //  var trace = {
-//         //     mode: "markers",
-//         //     marker: {
-//         //         color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-//         //         opacity: [1, 0.8, 0.6, 0.4],
-//         //         size: [40, 60, 80, 100]
-//         //       },
-//         //     name: name,
-//         //     x: otuIds[i],
-//         //     y: sampleValues[i]
-//         //  };
-//         // Plotly.newPlot("bubble", [trace]);
-//         // }
+function bubbleChart(bubbleId){
+  d3.json("data/samples.json").then(incomingData => {
     
-    
-        
-//         // console.log(otuIds[0]);
-//         // // console.log(sampleValues[0]);
-//         // console.log(otuIds.length);
-//         // console.log(sampleValues.length);
-        
-//     });
-    
-// };
+    var sampleData = incomingData.samples;
+    var sampleList = sampleData.filter(sampleObject => sampleObject.id == bubbleId);    
+    var finalList = sampleList[0];
 
-// bubbleChart();
+    var otuIds = finalList.otu_ids;
+    var sampleValues = finalList.sample_values;
+    var otuLables = finalList.otu_labels;
+  
 
-function bubbleChart(bubbleData){
-  d3.json("data/samples.json").then(function(data) {
-    var otuIds = data.samples[0].otu_ids;
-    var sampleValues = data.samples[0].sample_values;
-    var otuLables = data.samples[0].otu_labels;
-    
     trace = {
       mode: "markers",
       marker: {
@@ -156,22 +71,89 @@ function bubbleChart(bubbleData){
     };
 
     Plotly.newPlot("bubble", [trace]);
+  });
+};
+
+function gaugeChart(gaugeID){
+  d3.json("data/samples.json").then(incomingData => {
+    
+    var sampleData = incomingData.samples;
+    var sampleList = sampleData.filter(sampleObject => sampleObject.id == bubbleId);    
+    var finalList = sampleList[0];
+
+    var otuIds = finalList.otu_ids;
+    var sampleValues = finalList.sample_values;
+    var otuLables = finalList.otu_labels;
+
+
+//   var data = [
+//   {
+//     type: "indicator",
+//     mode: "gauge+number+delta",
+//     value: 420,
+//     title: { text: "Speed", font: { size: 24 } },
+//     delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+//      gauge: {
+//       axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
+//       bar: { color: "darkblue" },
+//       bgcolor: "white",
+//       borderwidth: 2,
+//       bordercolor: "gray",
+//       steps: [
+//         { range: [0, 250], color: "cyan" },
+//         { range: [250, 400], color: "royalblue" }
+//       ],
+//       threshold: {
+//         line: { color: "red", width: 4 },
+//         thickness: 0.75,
+//         value: 490
+//       }
+//     }
+//   }
+// ];
+
+// var layout = {
+//   width: 500,
+//   height: 400,
+//   margin: { t: 25, r: 25, l: 25, b: 25 },
+//   paper_bgcolor: "lavender",
+//   font: { color: "darkblue", family: "Arial" }
+// };
+
+// Plotly.newPlot('myDiv', data, layout);
+
 
   });
+}
+
+
+function init(){
+  var selector = d3.select("#selDataset");
+  d3.json("data/samples.json").then(incomingData => {
+    var sampleNames = incomingData.names;
+    sampleNames.forEach(sample => {
+      selector.append("option").text(sample).property("value", sample);
+      
+    });
+    var firstSample = sampleNames[0];
+    // console.log(firstSample);
+    demographicInfo(firstSample);
+    pieChart(firstSample);
+    gaugeChart(firstSample);
+    bubbleChart(firstSample);
+
+    
+
+    
+  });
+}
+
+function optionChanged(changedSample){
+  demographicInfo(changedSample);
+  pieChart(changedSample);
+  gaugeChart(changedSample);
+  bubbleChart(changedSample);
 
 }
 
-bubbleChart();
-
-
-// function demographicInfo() {
-//   var dropdownMenu = d3.select("#selDataset");
-//   var dataset = dropdownMenu.property("value");
-
-//   // console.log(dataset);
-
-
-
-// }
-    
-// demographicInfo();
+init()
